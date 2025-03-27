8f46e7804550da4950014e38b05d450b492fa4a5
Asadullah Ehsan <asadullahsuri131@gmail.com>
2025-03-27 17:04:42 +0500

    Beverage class

---
+++ DecoratorPattern/Beverage.cs
@@ -0,0 +1,14 @@
+﻿namespace DecoratorPattern;
+
+public abstract class Beverage
+{
+
+    private string description = "Unknown Beverage";
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+    public abstract double Cost();
+}
