6b0aff624213090efc3c15eb29030c6aa80ea7d3
Asadullah Ehsan <asadullahsuri131@gmail.com>
2025-03-27 20:56:03 +0500

    testing the implementation

--- DecoratorPattern/Program.cs
+++ DecoratorPattern/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("The Decorator Pattern\n");
+﻿using DecoratorPattern;
+
+Console.WriteLine("Decorator Pattern\n\n");
+
+Beverage beverage = new Espresso();
+beverage = new Mocha(beverage);
+Console.WriteLine(beverage.Description + " - $" + beverage.Cost());
+
+Beverage beverage2 = new DarkRoast();
+beverage2 = new Mocha(beverage2);
+//beverage2 = new Mocha(beverage2);
+//beverage2 = new Whip(beverage2);
+Console.WriteLine(beverage2.Description + " - $" + beverage2.Cost());
+
